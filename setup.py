8aacc67e7fab353fe99c4b5b4829569ceeaac598
nisithsinghAAI <68220280+nisithsinghAAI@users.noreply.github.com>
2023-03-27 13:53:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
 install_requires = [x.strip() for x in all_reqs]
 
 setup(name='veritastool',version='1.1.0',description ='Veritas Diagnosis tool for fairness assessment.',author='MAS Veritas',
-install_requires=install_requires,python_requires='>=3.8.x,<=3.10.x',
+install_requires=install_requires,python_requires='>=3.8.0,<=3.10.10',
 packages=['veritastool.model','veritastool.principles','veritastool.metrics','veritastool.config','veritastool.util','veritastool.resources','veritas.usecases'],
 include_package_data=True,
 zip_safe=False)
